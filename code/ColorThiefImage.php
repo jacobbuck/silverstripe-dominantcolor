adf0337d501e2a6377a15a8848d5087060bd5149
Jacob Buck <buck.jacob@gmail.com>
2015-08-14 14:56:38 +1200

    Add image extension

---
+++ code/ColorThiefImage.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Decorates {@link Image} to return ratio
+ *
+ * @package bnz
+ * @subpackage mysite
+ */
+
+
+class ColorThiefImage extends Extension {
+
+	/**
+	 * @return string
+	 */
+	public function getColor () {
+		return '#fff';
+	}
+
+}
