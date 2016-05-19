8fa4170755c6c3303ae1d7d4290f027b755ef62b
Jacob Buck <buck.jacob@gmail.com>
2016-05-19 16:17:02 +1200

    Convert ColorfulImage model into a DataExtension. Store # with color value

--- code/ColorfulImageExtension.php
+++ code/ColorfulImageExtension.php
@@ -8,10 +8,10 @@
 
 use ColorThief\ColorThief;
 
-class ColorfulImage extends Image {
+class ColorfulImageExtension extends DataExtension {
 
 	private static $db = array(
-		'Color' => 'Varchar(6)'
+		'Color' => 'Varchar(16)'
 	);
 
 	/**
@@ -19,8 +19,8 @@ class ColorfulImage extends Image {
 	 * @return String
 	 */
 	public function getColor() {
-		$color = $this->getField('Color');
-		if ($color === NULL) {
+		$color = $this->owner->getField('Color');
+		if ($color === null) {
 			$color = $this->dominantColor();
 		}
 		return $color;
@@ -38,14 +38,15 @@ class ColorfulImage extends Image {
 	 * Add cms field for Color db field
 	 * @return FieldList
 	 */
-	public function getCMSFields() {
-		$fields = parent::getCMSFields();
-
+	public function updateCMSFields(FieldList $fields) {
+		$color = $this->owner->Color;
 		$pallete = $this->dominantColorPalette();
 
 		$options = array();
+		$options[$color] = $color;
+
 		foreach ($pallete as $color) {
-			$options[$color] = "#$color";
+			$options[$color] = $color;
 		}
 
 		$fields->AddFieldToTab('Root.Main',
@@ -53,29 +54,17 @@ class ColorfulImage extends Image {
 				$name = 'Color',
 				$title = 'Color',
 				$source = $options,
-				$value = $this->Color
+				$value = $this->owner->Color
 			)
 		);
-
-		return $fields;
-	}
-
-	/**
-	 * Set the Color field to the dominant color by default
-	 */
-	public function populateDefaults() {
-		if (!$this->Filename && $this->getField('Color') === NULL) {
-			$this->Color = $this->dominantColor();
-		}
-		parent::populateDefaults();
 	}
 
 	/**
 	 * Write the Color field to get the dominant color if not set
 	 */
 	public function onBeforeWrite() {
-		if ($this->getField('Color') === NULL) {
-			$this->Color = $this->dominantColor();
+		if ($this->owner->getField('Color') === null) {
+			$this->owner->Color = $this->dominantColor();
 		}
 		parent::onBeforeWrite();
 	}
@@ -86,8 +75,8 @@ class ColorfulImage extends Image {
 	 */
 	public function dominantColor() {
 		$c = ColorThief::getColor(
-			$sourceImage = $this->getFullPath(),
-			$quality = $this->config()->quality
+			$sourceImage = $this->owner->getFullPath(),
+			$quality = Config::inst()->get($this->class, 'quality')
 		);
 		return self::array_to_hex($c);
 	}
@@ -99,9 +88,9 @@ class ColorfulImage extends Image {
 	 */
 	public function dominantColorPalette($colorCount = 5) {
 		$c = ColorThief::getPalette(
-			$sourceImage = $this->getFullPath(),
+			$sourceImage = $this->owner->getFullPath(),
 			$colorCount = $colorCount,
-			$quality = $this->config()->quality
+			$quality = Config::inst()->get($this->class, 'quality')
 		);
 		return array_map(array(get_class($this), 'array_to_hex'), $c);
 	}
@@ -109,7 +98,7 @@ class ColorfulImage extends Image {
 	/**
 	 * Get contrast color of a passed color
 	 * @see https://24ways.org/2010/calculating-color-contrast/
-	 * @param {Array|String} $color
+	 * @param Array|String $color
 	 * @return String 'black' or 'white'
 	 */
 	public static function get_contrast_yiq($color) {
@@ -122,23 +111,24 @@ class ColorfulImage extends Image {
 
 	/**
 	 * Converts a color array into a hex string
-	 * @param $color (array) (red, blue, green)
+	 * @param Array $color (red, blue, green)
 	 * @return String
 	 */
 	protected static function array_to_hex($color) {
 		if (empty($color)) {
-			return NULL;
+			return null;
 		}
 		$hex = dechex(($color[0]<<16)|($color[1]<<8)|$color[2]);
-		return str_pad($hex, 6, '0', STR_PAD_LEFT);
+		return '#' . str_pad($hex, 6, '0', STR_PAD_LEFT);
 	}
 
 	/**
 	 * Converts a hex string to color array
-	 * @param $color (string)
-	 * @return (array) (red, blue, green)
+	 * @param String $color
+	 * @return Array (red, blue, green)
 	 */
 	protected static function hex_to_array($color) {
+		$color = ltrim($color, '#');
 		$r = hexdec(substr($color, 0, 2));
 		$g = hexdec(substr($color, 2, 2));
 		$b = hexdec(substr($color, 4, 2));
