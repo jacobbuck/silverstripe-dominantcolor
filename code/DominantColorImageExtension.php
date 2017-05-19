39472094a1f580632cfff919f944bcadaae85820
Jacob Buck <buck.jacob@gmail.com>
2017-05-19 21:07:41 +1200

    Rename ColorfulImageExtension to DominantColorImageExtension. Refactor to scope to only picking the dominant color and contrasting color. Add caching.

---
+++ code/DominantColorImageExtension.php
@@ -0,0 +1,78 @@
+<?php
+
+use ColorThief\ColorThief;
+
+class DominantColorImageExtension extends Extension
+{
+
+    /**
+     * Get contrast color to `Color` field
+     * @return String 'black' or 'white'
+     */
+    public function getContrastColor()
+    {
+        return self::contrastYIQ($this->dominantColor());
+    }
+
+    /**
+     * Get the primary dominant color of this Image
+     * @return String
+     */
+    public function getDominantColor()
+    {
+        return self::toHexString($this->dominantColor());
+    }
+
+    /**
+     * Get the primary dominant color of this Image
+     * @return Array (red, blue, green)
+     */
+    protected function dominantColor()
+    {
+        $sourceImage = $this->owner->getFullPath();
+
+        $cache = SS_Cache::factory(get_called_class());
+        $cacheKey = md5($this->owner->ID . $sourceImage);
+
+        $cached = $cache->load($cacheKey);
+
+        if ($cached) {
+            return explode(',', $cached);
+        }
+
+        $color = ColorThief::getColor(
+            $sourceImage = $sourceImage,
+            $quality = Config::inst()->get(get_called_class(), 'quality')
+        );
+
+        $cache->save(implode(',', $color), $cacheKey);
+
+        return $color;
+    }
+
+    /**
+     * Converts a color array into a hex string
+     * @param Array $color (red, blue, green)
+     * @return String
+     */
+    protected static function toHexString($color)
+    {
+        if (empty($color)) {
+            return null;
+        }
+        $hex = dechex(($color[0]<<16)|($color[1]<<8)|$color[2]);
+        return '#' . str_pad($hex, 6, '0', STR_PAD_LEFT);
+    }
+
+    /**
+     * Get contrast color of a passed color
+     * @see https://24ways.org/2010/calculating-color-contrast/
+     * @param Array $color
+     * @return String 'black' or 'white'
+     */
+    protected static function contrastYIQ($color)
+    {
+        $yiq = (($color[0]*299)+($color[1]*587)+($color[2]*114))/1000;
+        return ($yiq >= 128) ? 'black' : 'white';
+    }
+}
